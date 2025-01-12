78ef70ef70597037e5b07c59f98ea0351fc30bed
descobar <descobar@tenantevaluation.com>
2025-01-12 16:49:51 -0500

    Added final solution

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -6,6 +6,41 @@ Escriba el codigo que ejecute la accion solicitada en cada pregunta.
 """
 
 
+import pandas as pd
+from pathlib import Path
+import os
+
+# Path files
+train = Path(__file__).resolve().parents[1].joinpath("./files/input/train")
+test = Path(__file__).resolve().parents[1].joinpath("./files/input/test")
+
+
+def create_csv_from_dataset(path, output_filename):
+    # Create csv
+    output_dir_path = Path(__file__).resolve().parents[1].joinpath("./files/output")
+    output_file_path = output_dir_path.joinpath(f"./{output_filename}.csv")
+    
+    # Create output directory
+    try:
+        os.mkdir(output_dir_path)
+    except:
+        pass
+    with open(output_file_path, "w") as f:
+        f.write("phrase,target\n")
+
+    # Feed csv
+    for dir_name in os.listdir(path):
+        files_path = path.joinpath(dir_name)
+        files = os.listdir(files_path)
+        for file in files:
+            current_txt_file_path = files_path.joinpath(file)
+            with open(current_txt_file_path, "r") as f:
+                line = f.readlines()[0]
+            
+            with open(output_file_path, "a") as f:
+                f.write(f"\"{line}\",\"{dir_name}\"\n")
+
+
 def pregunta_01():
     """
     La información requerida para este laboratio esta almacenada en el
@@ -68,6 +103,8 @@ def pregunta_01():
     |  3 | Both operating profit and net sales for the three-month period increased , respectively from EUR16 .0 m and EUR139m , as compared to the corresponding quarter in 2006 | positive |
     |  4 | Tampere Science Parks is a Finnish company that owns , leases and builds office properties and it specialises in facilities for technology-oriented businesses         | neutral  |
     ```
-
-
     """
+    create_csv_from_dataset(train, "train_dataset")
+    create_csv_from_dataset(test, "test_dataset")
+
+pregunta_01()
